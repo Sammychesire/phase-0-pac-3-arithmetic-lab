acf800bde30a39fcd7854e9d83290445a2d51cc9
bob <sammy.chesire@student.moringaschool.com>
2022-08-03 13:14:40 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,45 @@
+let a=Math.floor(Math.random() * 1000)
+let b=Math.floor(Math.random() * 1000)
+
+function add(){
+
+}
+function subtract(){
+
+}
+function divide(){
+
+}
+function multiply(){
+
+}
+function add(a, b){
+a+b
+return a+b
+}
+function subtract(a, b){
+a-b
+return a-b
+}
+function multiply(a, b){
+a*b
+return a*b
+}
+function divide(a, b){
+a/b
+return a/b
+}
+function increment(n){
+    n++
+    return n
+}
+function decrement(n){
+    n--
+    return n
+}
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
